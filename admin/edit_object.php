sauruscms/Saurus-CMS-Community-Edition

commit cf0696ae3e6b64090486ed4dae4317067b158566

Search: remove excess spaces from the content search field, for better 'LIKE' search accuracy

admin/edit_object.php
@@ -663,6 +663,8 @@ else {
 
 		$pealkiri_strip = strip_tags($pealkiri_strip);
 		$sisu_strip = strip_tags($sisu_strip);
+		// remove excess spaces
+		$sisu_strip = preg_replace('/\s+/', ' ',$foo);
 
 		// overwrite catch for files, this is here so when a new file is being uploaded but
 		// a file with a same name already exists

commit 0f4f899322265464dbc6695d4fa68ad05b5cb1df

Fixed: the search field value was empty caused by commit cf0696ae3e6b64090486ed4dae4317067b158566

admin/edit_object.php
@@ -664,7 +664,7 @@ else {
 		$pealkiri_strip = strip_tags($pealkiri_strip);
 		$sisu_strip = strip_tags($sisu_strip);
 		// remove excess spaces
-		$sisu_strip = preg_replace('/\s+/', ' ',$foo);
+		$sisu_strip = preg_replace('/\s+/', ' ', $sisu_strip);
 
 		// overwrite catch for files, this is here so when a new file is being uploaded but
 		// a file with a same name already exists